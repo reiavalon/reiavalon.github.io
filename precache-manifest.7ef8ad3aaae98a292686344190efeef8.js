430e0c98287ef18995ebb8f0e44e8744f763e7f0
Ryan Peeples <reiavalon@gmail.com>
2021-02-26 12:20:36 -0800

    Frontend Change

---
+++ precache-manifest.7ef8ad3aaae98a292686344190efeef8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "46a5e4af875f34469f05",
+    "url": "/js/chunk-vendors.665be9fc.js"
+  },
+  {
+    "revision": "cb480c5c9a2b7abe814f",
+    "url": "/js/chunk-d2c0e04a.8aa99541.js"
+  },
+  {
+    "revision": "6c4aea674e646311bafc",
+    "url": "/js/chunk-71b9cd64.70bd89fc.js"
+  },
+  {
+    "revision": "ebdd36eb0e625f4663eb",
+    "url": "/js/app.7833741e.js"
+  },
+  {
+    "revision": "fd42824b734e17fba52a238544dcc24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb480c5c9a2b7abe814f",
+    "url": "/css/chunk-d2c0e04a.9d15e52e.css"
+  },
+  {
+    "revision": "6c4aea674e646311bafc",
+    "url": "/css/chunk-71b9cd64.3f68a4ca.css"
+  },
+  {
+    "revision": "ebdd36eb0e625f4663eb",
+    "url": "/css/app.566cf762.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a26ee08127a8cc9ac2b9d58a89472ed4.js"
+  "/precache-manifest.7ef8ad3aaae98a292686344190efeef8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "warrior_app"});
